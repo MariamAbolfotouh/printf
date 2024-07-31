313eef715f73cbee980e68de1bf865fe61e3e293
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-07-31 13:26:29 -0700

    fd

--- _printf.c
+++ _printf.c
@@ -2,46 +2,34 @@
 
 /**
  * _printf - produces output according to a format
- *
  * @format: character string
- *
  * Return: number of characters printed
  */
-
 int _printf(const char *format, ...)
 {
 	va_list arg;
 	int count = 0;
 	int i;
-	char *str;
+	char *s;
 
 	va_start(arg, format);
-
 	for (i = 0; format[i]; i++)
 	{
 		if (format[i] == '%')
 		{
 			i++;
 			if (format[i] == 'c')
-			{
 				count = count + write(1, &(va_arg(arg, int)), 1);
-			}
 			else if (format[i] == 's')
 			{
-				str = va_arg(arg, char *);
-				if (str)
-				{
-					count = count + write(1, str, _strlen(str));
-				}
+				s = va_arg(arg, char *);
+				if (s)
+					count = count + write(1, str, _strlen(s));
 				else
-				{
 					count = count + write(1, "(null)", 6);
-				}
 			}
 			else if (format[i] == '%')
-			{
 				count = count + write(1, '%', 1);
-			}
 			else
 			{
 				count = count + write(1, &format[i - 1], 1);
