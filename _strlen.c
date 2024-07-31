24f81189ffd4785183cb5bd6b7f7776d60d8d9ce
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-07-31 16:31:56 -0700

    f

--- _strlen.c
+++ _strlen.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -19,4 +18,49 @@ int _strlen(char *s)
 	}
 
 	return (len);
+}
+
+/**
+ * _isdigit - checks if char is digit
+ *
+ * @c: char to be checked
+ *
+ * Return: 1 if digit
+ */
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+/**
+ * num_print - print num
+ *
+ * @str: num as str
+ * @parameters: parameters struct
+ *
+ * Return: chars
+ */
+
+int num_print(char *str, parameters_t *parameters)
+{
+	unsigned int i = _strlen(str);
+	int j = (!parameters->unsign && *str == '-');
+
+	if (!parameters->precision && *str == '0' && !str[1])
+		str = "";
+	if (j)
+	{
+		str++;
+		i--;
+	}
+	if (parameters->precision != UNIT_MAX)
+		while (i++ < parameters->precision)
+			*--str= '0';
+	if (j)
+		*--str = '-';
+	if (!parameters->minus_flag)
+		return (num_print_right(str, parameters));
+	else
+		return (num_print_left(str, parameters));
 }
---
+++ convert.c
@@ -0,0 +1,86 @@
+#include "main.h"
+
+/**
+ * conv - converter function
+ *
+ * @number: num
+ * @base: base
+ * @flag: arg flags
+ * @parameters: parameters struct
+ *
+ * Return: str
+ */
+
+char *conv(long int number, int base, int flag, parameters_t *parameters)
+{
+	static char *arr;
+	static char buf[50];
+	char *p;
+	char sign = 0;
+	unsigned long l = number;
+	(void)parameters;
+
+	if (!(flag & CONVERT_UNSIGNED) && number < 0)
+	{
+		l = -number;
+		sign = '-';
+	}
+	arr = flag & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+	p = &buf[49];
+	*p = '\0';
+
+	do {
+		*--p = arr[l % base];
+		l /= base;
+	} while (l != 0);
+
+	if (sign)
+		*--p = sign;
+	return (p);
+}
+
+/**
+ * unsigned_print - print unsigned integers numbs
+ *
+ * @p: pointer to arg
+ * @parameters: parameters struct
+ *
+ * Return: bytes
+ */
+
+int unsigned_print(va_list p, parameters_t *parameters)
+{
+	unsigned long l;
+
+	if (parameters->l_mod)
+		l = (unsigned long)va_arg(p, unsigned long);
+	else if (parameters->h_mod)
+		l = (unsigned short int)va_arg(p, unsigned int);
+	else
+		l = (unsigned int)va_arg(p, unsigned int);
+	parameters->unsign = 1;
+	return (num_print(conv(1, 10, CONVERT_UNSIGNED,parameters), parameters));
+}
+
+/**
+ * address_print - print address
+ *
+ * @p: pointer to arg
+ * @parameters: parameters struct
+ *
+ * Return: bytes
+ */
+
+int address_print(va_list p, parameters_t *parameters)
+{
+	unsigned long int i = va_arg(p, unsigned long int);
+	char *s;
+
+	if (!i)
+		return (_puts("(nil)"));
+
+	s = conv(i, 16, CONVERT_UNSIGNED | CONVERT_LOWERCASE, parameters);
+	*--s = 'x';
+	*--s = '0';
+	return (num_print(s, parameters));
+}
