9f179058472952c43cbe9bf8f47a41290d5459db
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-08-07 05:58:49 -0700

    fd

--- _printf.c
+++ _printf.c
@@ -9,12 +9,12 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list arg;
+	va_list p;
 	int count = 0;
 	char *s, *i;
 	params_t params = PARAMETERS_INITIATION;
 
-	va_start(arg, format);
+	va_start(p, format);
 
 	if ((format[0] == '%' && !format[1]) || !format)
 		return (-1);
@@ -22,7 +22,7 @@ int _printf(const char *format, ...)
 		return (-1);
 	for (s = (char *)format; *s; s++)
 	{
-		parameters_initial(&params, arg);
+		parameters_initial(&params, p);
 		if (*s != '%')
 		{
 			count = count + _putchar(*s);
@@ -34,17 +34,17 @@ int _printf(const char *format, ...)
 		{
 			s++;
 		}
-		s = got_width(s, &params, arg);
-		s = got_prec(s, &params, arg);
+		s = got_width(s, &params, p);
+		s = got_prec(s, &params, p);
 		if (!got_modifier(s, &params))
 			s++;
 		if (!got_spec(s))
 			count = count + from_to(i, s, params.l_mod ||
 				params.h_mod ? s - 1 : 0);
 		else
-			count = count + got_print(s, arg, &params);
+			count = count + got_print(s, p, &params);
 	}
 	_putchar(buffer_flush);
-	va_end(arg);
+	va_end(p);
 	return (count);
 }
