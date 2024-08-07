f971f5374688d41205026ffa00db6519e9e11df9
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-08-07 15:05:05 -0700

    df

--- _printf.c
+++ _printf.c
@@ -7,44 +7,121 @@
  *
  * Return: number of characters printed
  */
+
 int _printf(const char *format, ...)
 {
 	va_list p;
 	int count = 0;
-	char *s, *i;
-	params_t params = PARAMETERS_INITIATION;
+	int i = 0;
+	int n;
+	char ch;
+	char *s;
 
 	va_start(p, format);
 
-	if ((format[0] == '%' && !format[1]) || !format)
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (s = (char *)format; *s; s++)
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		parameters_initial(&params, p);
-		if (*s != '%')
+		if (format[i] == '%')
 		{
-			count = count + _putchar(*s);
-			continue;
+			i++;
+			if (format[i] == 'c')
+			{
+				ch = va_arg(p, int);
+				count = count + _putchar(ch);
+			}
+			else if (format[i] == 's')
+			{
+				s = va_arg(p, char *);
+				count = count + S_print(s);
+			}
+			else if (format[i] == 'd')
+			{
+				n = va_arg(p, int);
+				count = count + int_print(n);
+			}
+			else
+				count = count + _putchar(format[i]);
 		}
-		i = s;
-		s++;
-		while (got_flag(s, &params))
-		{
-			s++;
-		}
-		s = got_width(s, &params, p);
-		s = got_prec(s, &params, p);
-		if (!got_modifier(s, &params))
-			s++;
-		if (!got_spec(s))
-			count = count + from_to(i, s, params.l_mod ||
-				params.h_mod ? s - 1 : 0);
 		else
-			count = count + got_print(s, p, &params);
+			count = count + _putchar(format[i]);
 	}
-	_putchar(buffer_flush);
+
 	va_end(p);
 	return (count);
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * S_print - print string
+ *
+ * @str: string to be printed
+ *
+ * Return: no. of bytes
+ */
+
+int S_print(char *str)
+{
+	int i = 0, j;
+
+	while (str[i] != '\0')
+	{
+		j = _putchar(str[i]);
+		if (j == -1)
+			return (-1);
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * int_r_print - print integer
+ *
+ * @n: num to be printed
+ *
+ * Return: no. of bytes
+ */
+
+int int_r_print(int n)
+{
+	int i = 0, j;
+
+	if (n == 0)
+		return (0);
+	if (n < 0)
+	{
+		i = _putchar('-');
+		n = n * -1;
+	}
+
+	j = int_r_print(n / 10) + 1;
+	_putchar('0' + (n % 10));
+
+	return (i + j);
+}
+
+/**
+ * int_print - print int
+ *
+ * @n: num to be printed
+ *
+ * Return: no
+ */
+
+int int_print(int n)
+{
+	if (n == 0)
+		return (_putchar('0'));
+	return (int_r_print(n));
 }
--- main.h
+++ main.h
@@ -7,95 +7,10 @@
 #include <stdarg.h>
 #include <limits.h>
 
-#define op_buffer_size 1024
-#define buffer_flush -1
-
-#define NULL_STRING "(null)"
-#define PARAMETERS_INITIATION {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-
-#define CONVERT_LOWERCASE	1
-#define CONVERT_UNSIGNED	2
-
-/**
- * struct parameters - the parameters of struct
- *
- * @unsign: if unsigned value
- * @plus_flag: if [+] flag specified
- * @minus_flag: if [-] flag specified
- * @space_flag: if [space] flag specified
- * @hashtag_flag: if [#] flag specified
- * @zero_flag: if [zero] flag specified
- * @width: width of field specified
- * @precision: precision of field specified
- * @h_mod: if [h] specified
- * @l_mod: if [l] specified
- */
-
-typedef struct parameters
-{
-	unsigned int unsign	: 1;
-	unsigned int plus_flag	: 1;
-	unsigned int minus_flag	: 1;
-	unsigned int space_flag	: 1;
-	unsigned int hashtag_flag	: 1;
-	unsigned int zero_flag	: 1;
-	unsigned int width;
-	unsigned int precision;
-	unsigned int h_mod	: 1;
-	unsigned int l_mod	: 1;
-} params_t;
-
-/**
- * struct specifier - specifier
- *
- * @specifier: format
- * @f: function
- */
-
-typedef struct specifier
-{
-	char *specifier;
-	int (*f)(va_list, params_t *);
-} specifier_t;
-
-int _puts(char *str);
-int _putchar(int c);
-
-int int_print(va_list p, params_t *params);
-int char_print(va_list p, params_t *params);
-int string_print(va_list p, params_t *params);
-int percent_print(va_list p, params_t *params);
-int S_print(va_list p, params_t *params);
-
-int unsigned_print(va_list p, params_t *params);
-int address_print(va_list p, params_t *params);
-char *conv(long int number, int base, int flag, params_t *params);
-
-int (*got_spec(char *s))(va_list p, params_t *params);
-int got_print(char *s, va_list p, params_t *params);
-int got_flag(char *s, params_t *params);
-int got_modifier(char *s, params_t *params);
-char *got_width(char *s, params_t *params, va_list p);
-
-int hex_print(va_list p, params_t *params);
-int HEX_print(va_list p, params_t *params);
-int bin_print(va_list p, params_t *params);
-int oct_print(va_list p, params_t *params);
-
-int _strlen(char *s);
-int num_print(char *str, params_t *params);
-int _isdigit(int c);
-int num_print_right(char *str, params_t *params);
-int num_print_left(char *str, params_t *params);
-
-void parameters_initial(params_t *params, va_list p);
-
-int from_to(char *from, char *to, char *except);
-int rev_print(va_list p, params_t *params);
-int rot13_print(va_list p, params_t *params);
-
-char *got_prec(char *s, params_t *params, va_list p);
-
 int _printf(const char *format, ...);
+int _putchar(char c);
+int S_print(char *str);
+int int_r_print(int n);
+int int_print(int n);
 
 #endif
